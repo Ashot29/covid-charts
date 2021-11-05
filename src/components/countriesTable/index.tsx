2086423a57ad08176e1694545a9fedc948865844
Ashot <sargsyan.ashot19@gmail.com>
2021-11-05 16:17:23 +0400

    Tables are sortable

--- src/components/countriesTable/index.tsx
+++ src/components/countriesTable/index.tsx
@@ -9,13 +9,37 @@ import Paper from "@mui/material/Paper";
 import { useSelector } from "react-redux";
 import { RootState } from "../../stateManagement/reducers/rootReducer";
 import { ICountry } from "./../../stateManagement/reducers/countriesReducer";
-import SortIcon from '@mui/icons-material/Sort';
+import SortIcon from "@mui/icons-material/Sort";
 import "./index.css";
 
+const sortTypes: any = {
+  totalCases: {
+    fn: (a: ICountry, b: ICountry) => b.confirmed - a.confirmed,
+  },
+  countryName: {
+    fn: (a: ICountry, b: ICountry) =>
+      a.country > b.country ? 1 : b.country > a.country ? -1 : 0,
+  },
+  newCases: {
+    fn: (a: ICountry, b: ICountry) => b.confirmed_daily - a.confirmed_daily,
+  },
+  totalDeaths: {
+    fn: (a: ICountry, b: ICountry) => b.deaths - a.deaths,
+  },
+  newDeaths: {
+    fn: (a: ICountry, b: ICountry) => b.deaths_daily - a.deaths_daily,
+  }
+};
+
 export default function CountriesTable() {
+  const [sortState, setSortState] = React.useState("totalCases");
   const countries = useSelector(
     (state: RootState) => state.countries.countries
   );
+
+  function changeSortState(event: any) {
+    setSortState(event.target.dataset.name);
+  }
 
   return (
     <TableContainer component={Paper}>
@@ -26,50 +50,87 @@ export default function CountriesTable() {
               <span>#</span>
             </TableCell>
             <TableCell>
-              <span className="table-cell-span header-cell">Country <span><SortIcon /></span></span>
+              <span
+                className="table-cell-span header-cell"
+                data-name="countryName"
+                onClick={changeSortState}
+              >
+                Country{" "}
+                <span>
+                  <SortIcon />
+                </span>
+              </span>
             </TableCell>
             <TableCell>
-              <span className="table-cell-span header-cell">Total Cases <span><SortIcon /></span></span>
+              <span
+                className="table-cell-span header-cell"
+                data-name="totalCases"
+                onClick={changeSortState}
+              >
+                Total Cases{" "}
+                <span>
+                  <SortIcon />
+                </span>
+              </span>
             </TableCell>
             <TableCell>
-              <span className="table-cell-span header-cell">New Cases <span><SortIcon /></span></span>
+              <span className="table-cell-span header-cell" data-name="newCases" onClick={changeSortState}>
+                New Cases{" "}
+                <span>
+                  <SortIcon />
+                </span>
+              </span>
             </TableCell>
             <TableCell>
-              <span className="table-cell-span header-cell">Total Deaths <span><SortIcon /></span></span>
+              <span className="table-cell-span header-cell" data-name='totalDeaths' onClick={changeSortState}>
+                Total Deaths{" "}
+                <span>
+                  <SortIcon />
+                </span>
+              </span>
             </TableCell>
             <TableCell>
-              <span className="table-cell-span header-cell">New Deaths <span><SortIcon /></span></span>
+              <span className="table-cell-span header-cell" data-name='newDeaths' onClick={changeSortState}>
+                New Deaths{" "}
+                <span>
+                  <SortIcon />
+                </span>
+              </span>
             </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {countries.map((country: ICountry, index: number) => (
-            <TableRow
-              key={country.country}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell>
-                <span>{index + 1}</span>
-              </TableCell>
-              <TableCell component="th" scope="row">
-                <span className="table-cell-span">{country.country}</span>
-              </TableCell>
-              <TableCell>
-                <span className="table-cell-span">{country.confirmed}</span>
-              </TableCell>
-              <TableCell>
-                <span className="table-cell-span">
-                  {country.confirmed_daily}
-                </span>
-              </TableCell>
-              <TableCell>
-                <span className="table-cell-span">{country.deaths}</span>
-              </TableCell>
-              <TableCell>
-                <span className="table-cell-span">{country.deaths_daily}</span>
-              </TableCell>
-            </TableRow>
-          ))}
+          {[...countries]
+            .sort(sortTypes[sortState].fn)
+            .map((country: ICountry, index: number) => (
+              <TableRow
+                key={country.country}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell>
+                  <span>{index + 1}</span>
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  <span className="table-cell-span">{country.country}</span>
+                </TableCell>
+                <TableCell>
+                  <span className="table-cell-span">{country.confirmed}</span>
+                </TableCell>
+                <TableCell>
+                  <span className="table-cell-span">
+                    {country.confirmed_daily}
+                  </span>
+                </TableCell>
+                <TableCell>
+                  <span className="table-cell-span">{country.deaths}</span>
+                </TableCell>
+                <TableCell>
+                  <span className="table-cell-span">
+                    {country.deaths_daily}
+                  </span>
+                </TableCell>
+              </TableRow>
+            ))}
         </TableBody>
       </Table>
     </TableContainer>
